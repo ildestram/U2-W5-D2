70bd7dd99a807c9d8f928d37baa40041ee224fdf
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2022-09-27 16:04:44 +0200

    m

--- scriptTwo.js
+++ scriptTwo.js
@@ -1,6 +1,6 @@
-let firstAmmount = 100;
-let ammount = firstAmmount;
-let howYouSpent = 200;
+// let firstAmmount = 100;
+// let ammount = firstAmmount;
+// let howYouSpent = 200;
 
 // while (ammount > 0) {
 //     ammount = ammount - howYouSpent;
@@ -15,18 +15,33 @@ let howYouSpent = 200;
 //     }
 // }
 
-while (ammount > 0) {
-    ammount = ammount - howYouSpent;
+// while (ammount > 0) {
+//     ammount = ammount - howYouSpent;
 
-    if (ammount < firstAmmount / 2) {
-        alert("attenzione hai già speso metà del tuo budget!")
-    } else {
-        alert("tranquillo, continua a spendere")
-    } 
+//     if (ammount < firstAmmount / 2) {
+//         alert("attenzione hai già speso metà del tuo budget!")
+//     } else {
+//         alert("tranquillo, continua a spendere")
+//     } 
 
-    if (ammount > firstAmmount / 5) {
-        alert("attenzione, se spendi ancora non avrai più soldi")
-    } else {
-        alert("mi dispiace, hai finito tutti i soldi!")
+//     if (ammount > firstAmmount / 5) {
+//         alert("attenzione, se spendi ancora non avrai più soldi")
+//     } else {
+//         alert("mi dispiace, hai finito tutti i soldi!")
+//     }
+// }
+
+let budget = 500;
+
+function azzera() {
+    for (let i = budget; i > 0; i--) {
+        if (i === 300) {
+            document.getElementById("budget-metà").innerHTML = "Attenzione, sei a metà del tuo budget";
+        } else {
+            ""
+        }
+        if (i === 600) {
+            document.getElementById("budget-finito").innerHTML = "Hai finito il budget";
+        }
     }
 }
